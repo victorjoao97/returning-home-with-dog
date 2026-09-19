0320566734109dfa85d8ce7db73699bde0b66aaf
João Victor Nascimento <dev.joao.vn@outlook.com>
2026-09-18 22:19:08 -0300

    idk

--- scenes/after_accident.gd
+++ scenes/after_accident.gd
@@ -12,3 +12,7 @@ func _on_follow_component_stopped() -> void:
 
 func _on_floor_body_entered(_body: Node3D) -> void:
 	get_tree().reload_current_scene.call_deferred()
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	body.position.y = 20
